25e56d86d14ecd0783615ada4662f3201343b09d
Kayla Carpenter <aylkaraemi@gmail.com>
2018-11-09 17:41:33 -0700

    feat: add sql queries to LogsAnalysis.py

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -5,23 +5,39 @@ import psycopg2
 db_name = "news"
 
 
-def top_articles():
+def ask_queries():
     # retuns a list of the top 3 most popular articles rated by # of views
     db = psycopg2.connect(dbname=db_name)
     c = db.cursor()
-    c.execute("query")
+    c.execute("""
+                select title, views
+                    from article_info
+                    order by views desc
+                    limit 3
+                """)
     articles = c.fetchall()
+    c.execute("""
+                select sum(views) as total_views, name
+                    from article_info
+                    group by name
+                    order by total_views desc
+                """)
+    authors = c.fetchall()
+    c.execute("""
+                select date, (errors * 100.0 / total) as percent
+                    from daily_requests
+                    where (errors * 100.0 / total) > 1.0;
+                """)
+    request_errors = c.fetchall()
     db.close()
-    return articles
+    return articles, authors, request_errors
 
-def top_authors():
-    # returns a sorted list of the authors based on article views
-    pass
+def generate_report():
+    articles, authors, request_errors = ask_queries()
+    report = ''
+    print(articles + "\n" + authors + "\n" + request_errors)
 
-def request_errors():
-    # returns a list of the days on which more than 1% of HTTP requests resulted
-        # in errors
-    pass
+
 
 if __name__ == '__main__':
     pass
